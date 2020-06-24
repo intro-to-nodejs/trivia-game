ba007b32b60951a0e17ba93a4d4b60828f6b54f8
kchia <kchia87@gmail.com>
2020-06-24 13:30:51 -0400

    step 6: working with Socket.IO events

--- public/js/trivia.js
+++ public/js/trivia.js
@@ -1,5 +1,8 @@
+const socket = io();
+
 const urlSearchParams = new URLSearchParams(window.location.search);
 const playerName = urlSearchParams.get('playerName');
+const room = urlSearchParams.get('room');
 
 /*
   WELCOME HEADER
@@ -13,3 +16,29 @@ document.querySelector('main').insertAdjacentHTML(
     playerName,
   })
 );
+
+/*
+  SOCKETIO JOIN EVENT EMITTER
+*/
+socket.emit('join', { playerName, room }, error => {
+  if (error) {
+    alert(error);
+    location.href = '/';
+  }
+});
+
+/*
+  SOCKETIO MESSAGE EVENT LISTENER
+*/
+socket.on('message', ({ playerName, text, createdAt }) => {
+  const chatMessages = document.querySelector('.chat__messages');
+  const messageTemplate = document.querySelector('#message-template').innerHTML;
+  const template = Handlebars.compile(messageTemplate);
+
+  const html = template({
+    playerName,
+    text,
+    createdAt: moment(createdAt).format('h:mm a'),
+  });
+  chatMessages.insertAdjacentHTML('afterBegin', html);
+});
