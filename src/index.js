de807c2837be916e19f050dbd69da7b5c2fcbca2
kchia <kchia87@gmail.com>
2020-06-24 11:17:59 -0400

    step 3: connect socket.io to express server

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const publicDirectoryPath = path.join(__dirname, '../public');
 app.use(express.static(publicDirectoryPath));
 
+io.on('connection', () => {
+  console.log('A new player just connected');
+});
+
 const port = process.env.PORT || 8080;
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is up on port ${port}.`);
 });
